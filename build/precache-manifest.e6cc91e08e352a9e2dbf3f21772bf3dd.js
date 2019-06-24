bfbf88b5d82e86d5229bfcff557e5e2590492abe
Madeline Zechar <maddie.zechar@Madelines-MacBook-Pro.local>
2019-06-24 12:59:48 -0400

    first commit

---
+++ build/precache-manifest.e6cc91e08e352a9e2dbf3f21772bf3dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ddf7b65fd55cffd79b7",
+    "url": "/static/css/main.d2c98b4b.chunk.css"
+  },
+  {
+    "revision": "1ddf7b65fd55cffd79b7",
+    "url": "/static/js/main.1ddf7b65.chunk.js"
+  },
+  {
+    "revision": "85042e97a37d71d7ff89",
+    "url": "/static/js/1.85042e97.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0ebc18432c0a71cfd876a27933f91a4a",
+    "url": "/static/media/mongoleaf.0ebc1843.png"
+  },
+  {
+    "revision": "6c93bd2010905059e08c8b522adfc219",
+    "url": "/static/media/pixelatedLeaf.6c93bd20.svg"
+  },
+  {
+    "revision": "2381beb9f25b588c5ceaa73251868688",
+    "url": "/index.html"
+  }
+];
